f08cc2d5f5ec37c63104128bc4780e2c504ca409
Abde Manaaf Ghadiali <abdemanaaf.ghadiali.1998@gmail.com>
2023-11-11 14:49:32 -0500

    tabular library updated

--- src/main.v
+++ src/main.v
@@ -2,6 +2,7 @@ module main
 
 import time
 import tabular
+import utils
 import linear_regression as lr
 import py_plot
 
@@ -20,15 +21,13 @@ struct OLSRegressionDescription {
 
 
 fn main() {
-    excel_data := read_xlsx_v.parse('./data/test_data_mv.xlsx')!
+    excel_data := read_xlsx_v.parse('./data/test_data_mv_sample.xlsx')!
     mut input_data := excel_data.clone()
 
     zero_weight_bias := false
 
     column_names := excel_data.first()
     input_data.delete(0)
-
-    println("Features Used: ${column_names}")
 
     mut x := [][]f64{len: input_data[0].len - 1, init: []f64{len: input_data.len}}
     mut y := []f64{}
@@ -44,12 +43,49 @@ fn main() {
         }
     }
 
-    mut df := tabular.DataFrame.data_frame(x)
-    mut vec := tabular.Series.series(y)
+    mut df := tabular.DataFrame.data_frame(x, column_names[0..(column_names.len - 1)])
+    mut vec := tabular.Series.series(y, column_names[column_names.len - 1])
+    mut vec_x := tabular.Series.series(x[0], 'house_size_sqft')
 
     println(df)
     println(vec)
+
+    println(utils.fsum(y))
     println(vec.fsum())
+    println('')
+
+    println(utils.fmean(y))
+    println(vec.fmean())
+    println('')
+
+    println(utils.fvariance(y))
+    println(vec.fvariance())
+    println('')
+
+    println(utils.fcovariance(x[0], y))
+    println(vec.fcovariance(mut vec_x))
+    println('')
+
+    println(utils.fsum(x[0]))
+    println(df.fsum('house_size_sqft'))
+    println('')
+
+    println(utils.fmean(x[0]))
+    println(df.fmean('house_size_sqft'))
+    println('')
+
+    println(utils.fvariance(x[0]))
+    println(df.fvariance('house_size_sqft'))
+    println('')
+
+    println(utils.fcovariance(x[0], x[1]))
+    println(df.fcovariance('house_size_sqft', 'no_of_bedrooms'))
+    println('')
+
+    print(df.get('house_size_sqft'))
+
+
+
 
     // mut model := lr.LinearRegression.init_model(0.0000000001, 500, zero_weight_bias, x.len)
     // model = model.fit_model(x, y)
--- src/tabular.v
+++ src/tabular.v
@@ -4,29 +4,125 @@ pub struct DataFrame {
 	mut:
 		data [][]f64
 		shape []int
+		column_names []string
 }
 
 pub struct Series {
 	mut:
 		data []f64
 		shape []int
+		series_name string
 }
 
-pub fn DataFrame.data_frame (data [][]f64) DataFrame {
+pub fn DataFrame.data_frame (data [][]f64, column_names []string) DataFrame {
 	return DataFrame{
 		data: data
 		shape: [data[0].len, data.len]
+		column_names: column_names
 	}
 }
 
-pub fn Series.series (data []f64) Series {
+pub fn (mut df DataFrame) get (column_name string) Series {
+	mut feature_index := 0
+
+	for idx in 0.. df.shape[1]{
+		if column_name == df.column_names[idx] {
+			feature_index = idx
+			break
+		}
+	}
+
+	mut feature_vector := Series.series(df.data[feature_index], column_name)
+
+	return feature_vector
+}
+
+pub fn (mut df DataFrame) fsum (column_name string) f64 {
+	mut feature_index := 0
+
+	for idx in 0.. df.shape[1]{
+		if column_name == df.column_names[idx] {
+			feature_index = idx
+			break
+		}
+	}
+
+	mut feature_vector := Series.series(df.data[feature_index], column_name)
+
+	return feature_vector.fsum()
+}
+
+pub fn (mut df DataFrame) fmean (column_name string) f64 {
+	mut feature_index := 0
+
+	for idx in 0.. df.shape[1]{
+		if column_name == df.column_names[idx] {
+			feature_index = idx
+			break
+		}
+	}
+
+	mut feature_vector := Series.series(df.data[feature_index], column_name)
+
+	return feature_vector.fmean()
+}
+
+pub fn (mut df DataFrame) fvariance (column_name string) f64 {
+	mut feature_index := 0
+
+	for idx in 0.. df.shape[1]{
+		if column_name == df.column_names[idx] {
+			feature_index = idx
+			break
+		}
+	}
+
+	mut feature_vector := Series.series(df.data[feature_index], column_name)
+
+	return feature_vector.fvariance()
+}
+
+pub fn (mut df DataFrame) fcovariance (column_name_a string, column_name_b string) f64 {
+	mut feature_index_a := 0
+	mut feature_index_b := 0
+
+	for idx in 0.. df.shape[1]{
+		if column_name_a == df.column_names[idx] {
+			feature_index_a = idx
+			break
+		}
+	}
+
+	for idx in 0.. df.shape[1]{
+		if column_name_b == df.column_names[idx] {
+			feature_index_b = idx
+			break
+		}
+	}
+
+	mut feature_vector_a := Series.series(df.data[feature_index_a], column_name_a)
+	mut feature_vector_b := Series.series(df.data[feature_index_b], column_name_b)
+
+	return feature_vector_a.fcovariance(mut feature_vector_b)
+}
+
+/* ############################################ Series Functions ######################################################## */
+
+pub fn Series.series (data []f64, series_name string) Series {
 	return Series{
 		data: data
 		shape: [data.len, 1]
+		series_name: series_name
 	}
 }
 
-pub fn (mut series Series) fsum () f64 {
+pub fn (mut series Series) update_series() Series {
+	series.shape = [series.data.len, 1]
+
+	return series
+}
+
+pub fn (mut series Series) fsum() f64 {
 	mut final_sum := 0.0
 
 	for idx in 0..series.shape[0] {
@@ -34,4 +130,35 @@ pub fn (mut series Series) fsum () f64 {
 	}
 
 	return final_sum
+}
+
+pub fn (mut series Series) fmean() f64 {
+	return series.fsum() / series.shape[0]
+}
+
+pub fn (mut series Series) fvariance() (f64) {
+	mut residual_array := Series.series([], 'residual_array')
+	mean := series.fmean()
+
+	for idx in 0..series.shape[0] {
+		residual_array.data << (series.data[idx] - mean) * (series.data[idx] - mean)
+		residual_array.update_series()
+	}
+
+	return residual_array.fsum() / series.shape[0]
+}
+
+pub fn (mut series Series) fcovariance(mut input_series Series) (f64) {
+	assert series.shape == input_series.shape
+
+	mut covariance := 0.0
+
+	mean_x := series.fmean()
+	mean_y := input_series.fmean()
+
+	for idx in 0..series.shape[0] {
+		covariance = covariance + ((series.data[idx] - mean_x) * (input_series.data[idx] - mean_y))
+	}
+
+	return covariance / series.shape[0]
 }
